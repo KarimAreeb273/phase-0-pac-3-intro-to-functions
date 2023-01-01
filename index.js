1d70196dfe586d1866cc4544d22b160405c1ed84
Areeb Karim <areebakarim99@gmail.com>
2022-12-31 23:08:27 -0500

    commit tree d96a37a1d6e9b597097a0ed25be163aac3fa6db0

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Follow along with the examples here
+function sayHello() {
+    console.log("Hello!");
+  }
+  sayHello();
+  
+function sayHelloToGuadalupe() {
+  console.log("Hello, Guadalupe!");
+}
+sayHelloToGuadalupe();
